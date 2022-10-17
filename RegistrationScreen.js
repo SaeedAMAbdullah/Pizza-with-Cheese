dde449a21a4aeb287dd5c38d2ed0a805f2b182f9
NazymKY <111902655+NazymKY@users.noreply.github.com>
2022-10-17 10:00:46 +0400

    RegistrationScreen JS file

---
+++ RegistrationScreen.js
@@ -0,0 +1,303 @@
+import * as React from 'react';
+import { useState } from 'react';
+import Alert from '@mui/material/Alert';
+import CircularProgress from '@mui/material/CircularProgress';
+import FormControl from '@mui/material/FormControl';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+function RegistrationScreen(props) {
+
+    // The states are: 
+    // (1) null, (2) "client error", (3) "backend error", (4) "loading", (5) "success"
+
+    var [formState, setFormState] = useState(null);
+    var [errorsState, setErrorsState] = useState([]);
+
+
+    // 1. Declare variables (not defined)
+    var firstNameField;
+    var lastNameField;
+    var emailField;
+    var passwordField;
+    var avatarField;
+
+     
+    // Create a JS object like an HTML form element 
+    var formData = new FormData();
+
+    function attachFile(evt) {
+
+        // Creating an array from the files attached by user
+        var files = Array.from(evt.target.files);
+
+        files.forEach(
+            function(fileAttachment, index) {
+                formData.append(index, fileAttachment);
+            }
+        )
+    }
+
+
+    function register() {
+
+
+        // 2. Validate the fields
+        var errors = [];
+
+        if(firstNameField.value.length === 0) {
+            errors.push('Please enter your first name');
+        }
+
+        if(lastNameField.value.length === 0) {
+            errors.push('Please enter your last name');
+        }
+
+        if(emailField.value.length === 0) {
+            errors.push('Please enter your email');
+        }
+
+        if(passwordField.value.length === 0) {
+            errors.push('Please enter your password');
+        }
+
+        // 3. If any field is not validated, go to "client error"
+        if( errors.length > 0 ) {
+            setFormState("client error");
+            setErrorsState( errors );
+        }
+
+        // 4. If all fields are valid
+        else {
+            // 5. Go to "loading"
+            setFormState("loading");
+            setErrorsState([]);
+
+            // 6. Send data backend
+            formData.append('firstName', firstNameField.value);
+            formData.append('lastName', lastNameField.value);
+            formData.append('email', emailField.value);
+            formData.append('password', passwordField.value);
+
+            fetch(
+                `${process.env.REACT_APP_BACKEND_ENDPOINT}/users/register`,
+                {
+                    'method': 'POST',
+                    'body': formData
+                }
+            )
+            .then(
+                function(backendResponse) {
+                    // Convert the HTTP string response to JSON
+                    return backendResponse.json();
+                }
+            )
+            .then(
+                // 7. If backend sends success, go to "success"
+                function(jsonResponse) {
+                    if(jsonResponse.status === "ok") {
+                        console.log('backend response /users/register', jsonResponse)
+                        setFormState("success");
+                    }
+                    else {
+                        setFormState("backend error");
+                    }
+                }
+            )
+            .catch(
+                // 8. If backends sends error, go to "backend error"
+                function(backendError) {
+                    console.log('backendError at /users/register', backendError)
+                    setFormState("backend error");
+                }
+            )
+        }
+    }
+
+    function addListItem(str) {
+        return <li>{str}</li>
+    }
+
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+    
+  );
+}
+
+const theme = createTheme();
+
+export default function SignUp() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+   // <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box 
+          component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                inputRef={ 
+                  function( thisElement ){
+                      firstNameField = thisElement;
+                  } 
+              }
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                inputRef={ 
+                  function( thisElement ){
+                      lastNameField = thisElement;
+                  } 
+              }
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                inputRef={ 
+                  function( thisElement ){
+                      emailAddressField = thisElement;
+                  } 
+              }
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                inputRef={ 
+                  function( thisElement ){
+                      passwordField = thisElement;
+                  } 
+              }
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <RegistrationScreen sx={{ mt: 5 }} />
+
+        <Box display="flex">
+                
+                {
+                    formState !== "loading" &&
+                    <Button onClick={register} size="large" variant="contained">Send</Button>
+                }
+                
+                {
+                    formState === "loading" &&
+                    <CircularProgress />
+                }
+            </Box>
+
+            <Box mt={2}>
+
+                { 
+                    formState === "client error" &&
+                    <Alert severity="error">
+                        <ul>
+                        {
+                            errorsState.map(addListItem)
+                        }
+                        </ul>
+                    </Alert>
+                }
+
+                {
+                    formState === "success" &&
+                    <Alert severity="success">
+                        You have registered successfully!
+                    </Alert>
+                }
+            </Box>
+
+      </Container>
+//    </ThemeProvider>
+  );
+}
